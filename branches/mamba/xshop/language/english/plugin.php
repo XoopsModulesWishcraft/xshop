9394f052c2cfa37feb312d457e1690785c4d4111
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-08-22 11:59:27 +0000

    creating a branch for converted version to XOOPS 2.5 GUI

---
+++ branches/mamba/xshop/language/english/plugin.php
@@ -0,0 +1,124 @@
+<?php
+
+	// THIS IS FOR THE PLUGIN STRAPPING - DO NOT TRANSLATE OR ALTER UNLESS YOU ARE MAKING CHANGES TO THE PLUGIN STRAPPING ACL
+	
+
+	define('_PLUGIN_SHOP_MI_ADDRESS_MANUFACTURE', 'AddressManufacture');
+	define('_PLUGIN_SHOP_MI_ADDRESS_PLUGIN_SHOP', 'AddressShop');
+	define('_PLUGIN_SHOP_MI_ADDRESS_POSTAL', 'AddressPostal');
+	define('_PLUGIN_SHOP_MI_ADDRESS_DELIEVERY', 'AddressDelievery');
+	define('_PLUGIN_SHOP_MI_ADDRESS_ORDERBY', 'AddressOrderBy');
+	define('_PLUGIN_SHOP_MI_ADDRESS_SHOW', 'AddressShow');
+	define('_PLUGIN_SHOP_MI_ADDRESS_OTHER', 'AddressOther');
+	define('_PLUGIN_SHOP_MI_ADDRESS_RETURNEDGOODS', 'AddressReturnedGoods');
+	define('_PLUGIN_SHOP_MI_ADDRESS_FRAUD', 'AddressFraud');
+	define('_PLUGIN_SHOP_MI_ADDRESS_STOLENINMAIL', 'AddressStolenInMail');
+	define('_PLUGIN_SHOP_MI_ADDRESS_EXPRESSDELIEVERY', 'AddressExpressDelievery');
+	define('_PLUGIN_SHOP_MI_ADDRESS_NONE', 'AddressNone');
+	
+	define('_PLUGIN_SHOP_MI_CONTACTS_EMAIL', 'ContactsEmail');
+	define('_PLUGIN_SHOP_MI_CONTACTS_PHONE', 'ContactPhone');
+	define('_PLUGIN_SHOP_MI_CONTACTS_MOBILE', 'ContactMobile');
+	define('_PLUGIN_SHOP_MI_CONTACTS_FAX', 'ContactFax');
+	define('_PLUGIN_SHOP_MI_CONTACTS_PAGER', 'ContactPager');
+	define('_PLUGIN_SHOP_MI_CONTACTS_OTHER', 'ContactOther');
+	
+	define('_PLUGIN_SHOP_MI_DAYS_OPEN', 'DaysOpen');
+	define('_PLUGIN_SHOP_MI_DAYS_CLOSED', 'DaysClosed');
+	define('_PLUGIN_SHOP_MI_DAYS_DISPLAYONLY', 'DaysDisplayOnly');
+	define('_PLUGIN_SHOP_MI_DAYS_NOSALE', 'DaysNoSale');
+	define('_PLUGIN_SHOP_MI_DAYS_DISCOUNT', 'DaysDiscount');
+	define('_PLUGIN_SHOP_MI_DAYS_CALL', 'DaysCall');
+	define('_PLUGIN_SHOP_MI_DAYS_EMAIL', 'DaysEmail');
+	define('_PLUGIN_SHOP_MI_DAYS_OTHER', 'DaysOther');
+	
+	define('_PLUGIN_SHOP_MI_DISCOUNT_QUITSTOCK', 'DiscountQuitStock');
+	define('_PLUGIN_SHOP_MI_DISCOUNT_GENERAL', 'DiscountGeneral');
+	define('_PLUGIN_SHOP_MI_DISCOUNT_WHOLESALE', 'DiscountWholesale');
+	define('_PLUGIN_SHOP_MI_DISCOUNT_NOREORDER', 'DiscountNoReorder');
+	define('_PLUGIN_SHOP_MI_DISCOUNT_QUANITY', 'DiscountQuanity');
+	define('_PLUGIN_SHOP_MI_DISCOUNT_OTHER', 'DisountOther');
+	
+	define('_PLUGIN_SHOP_MI_GALLERY_CAT_LOGO', 'GalleryCatLogo');
+	define('_PLUGIN_SHOP_MI_GALLERY_MANU_LOGO', 'GalleryManufactureLogo');
+	define('_PLUGIN_SHOP_MI_GALLERY_PRODUCT', 'GalleryProduct');
+	define('_PLUGIN_SHOP_MI_GALLERY_SHOP_LOGO', 'GalleryShop');
+	define('_PLUGIN_SHOP_MI_GALLERY_SHIPPING_LOGO', 'GalleryShipping');
+	define('_PLUGIN_SHOP_MI_GALLERY_DISCOUNT_LOGO', 'GalleryDiscount');
+	define('_PLUGIN_SHOP_MI_GALLERY_ORDER_LOGO', 'GalleryOrder');
+	define('_PLUGIN_SHOP_MI_GALLERY_WATERMARK', 'GalleryWatermark');
+	
+	define('_PLUGIN_SHOP_MI_ITEMS_MENUITEMS', 'ItemsMenu');
+	define('_PLUGIN_SHOP_MI_ITEMS_LONGITEMS', 'ItemsLong');
+	define('_PLUGIN_SHOP_MI_ITEMS_BOTHITEMS', 'ItemsBoth');
+	define('_PLUGIN_SHOP_MI_ITEMS_RSSITEM', 'ItemsRSS');
+	define('_PLUGIN_SHOP_MI_ITEMS_RSSANDLONGITEM', 'ItemsRSSLong');
+	define('_PLUGIN_SHOP_MI_ITEMS_ALLITEMS', 'ItemsAll');
+	
+	define('_PLUGIN_SHOP_MI_MANU_PLACESTOCKORDER', 'ManufacturePlaceStockOrder');
+	define('_PLUGIN_SHOP_MI_MANU_DONTPLACESTOCKORDER', 'ManufactureDontPlaceStockOrder');
+	define('_PLUGIN_SHOP_MI_MANU_MANUALORDER', 'ManufactureOrder');
+	define('_PLUGIN_SHOP_MI_MANU_RAISETOPURCHASEOFFICER', 'ManufactureRaiseToPurchaseOrder');
+	define('_PLUGIN_SHOP_MI_MANU_APIORDERING', 'ManufactureAPIOrdering');
+	define('_PLUGIN_SHOP_MI_MANU_LOCAL', 'ManufactureLocal');
+	define('_PLUGIN_SHOP_MI_MANU_INTERSTATE', 'ManufactureInterstate');
+	define('_PLUGIN_SHOP_MI_MANU_OVERSEAS', 'ManufactureOverseas');
+	define('_PLUGIN_SHOP_MI_MANU_INTERNAL', 'ManufactureInternal');
+	define('_PLUGIN_SHOP_MI_MANU_UNKNOWN', 'ManufactureUnknown');
+	
+	define('_PLUGIN_SHOP_MI_ORDER_OPENORDER', 'OrderOpen');
+	define('_PLUGIN_SHOP_MI_ORDER_CLOSEDORDER', 'OrderClosed');
+	define('_PLUGIN_SHOP_MI_ORDER_CHECKEDOUT', 'OrderCheckedOut');
+	define('_PLUGIN_SHOP_MI_ORDER_GONETOINVOICING', 'OrderGoneToInvoice');
+	define('_PLUGIN_SHOP_MI_ORDER_OTHER', 'OrderOther');
+	define('_PLUGIN_SHOP_MI_ORDER_PAID', 'OrderPaid');
+	define('_PLUGIN_SHOP_MI_ORDER_UNPAID', 'OrderUnpaid');
+	define('_PLUGIN_SHOP_MI_ORDER_CANCELLED', 'OrderCancelled');
+	define('_PLUGIN_SHOP_MI_ORDER_FRAUDPAID', 'OrderFraudPaid');
+	define('_PLUGIN_SHOP_MI_ORDER_FRAUDUNPAID', 'OrderFraudUnpaid');
+	define('_PLUGIN_SHOP_MI_ORDER_FRAUDCANCELLED', 'OrderFraudCancelled');
+	
+	define('_PLUGIN_SHOP_MI_CONNOTE_EXPRESS', 'ConnoteExpress');
+	define('_PLUGIN_SHOP_MI_CONNOTE_STANDARD', 'ConnoteStandard');
+	define('_PLUGIN_SHOP_MI_CONNOTE_RETURNED', 'ConnoteReturned');
+	define('_PLUGIN_SHOP_MI_CONNOTE_BYSEA', 'ConnoteBySea');
+	define('_PLUGIN_SHOP_MI_CONNOTE_BYROAD', 'ConnoteByRoad');
+	define('_PLUGIN_SHOP_MI_CONNOTE_BYPLANE', 'ConnoteByPlane');
+	define('_PLUGIN_SHOP_MI_CONNOTE_BYTRAIN', 'ConnoteByTrain');
+	
+	define('_PLUGIN_SHOP_MI_WEIGHT_KILOS', 'WeightKilos');
+	define('_PLUGIN_SHOP_MI_WEIGHT_POUNDS', 'WeightPounds');
+	define('_PLUGIN_SHOP_MI_WEIGHT_OTHER', 'WeightOther');
+	
+	define('_PLUGIN_SHOP_MI_PRODUCTS_INSTOCK', 'ProductsInStock');
+	define('_PLUGIN_SHOP_MI_PRODUCTS_OUTSTOCK', 'ProductsOutStock');
+	define('_PLUGIN_SHOP_MI_PRODUCTS_NOREORDER', 'ProductsNoReorder');
+	define('_PLUGIN_SHOP_MI_PRODUCTS_QUITSTOCK', 'ProductsQuitStock');
+	define('_PLUGIN_SHOP_MI_PRODUCTS_SUPLUSSTOCK', 'ProductsSurplusStock');
+	define('_PLUGIN_SHOP_MI_PRODUCTS_SERVICE', 'ProductsService');
+	define('_PLUGIN_SHOP_MI_PRODUCTS_TANGIABLEITEM', 'ProductsTangiableItem');
+	define('_PLUGIN_SHOP_MI_PRODUCTS_SERVICEANDITEM', 'ProductsServiceItem');
+	
+	define('_PLUGIN_SHOP_MI_SHIPPING_LOCAL', 'ShippingLocal');
+	define('_PLUGIN_SHOP_MI_SHIPPING_INTERSTATE', 'ShippingInterstate');
+	define('_PLUGIN_SHOP_MI_SHIPPING_OVERSEAS', 'ShippingOverseas');
+	define('_PLUGIN_SHOP_MI_SHIPPING_INTERNAL', 'ShippingInternal');
+	define('_PLUGIN_SHOP_MI_SHIPPING_BYSEA', 'ShippingBySea');
+	define('_PLUGIN_SHOP_MI_SHIPPING_BYROAD', 'ShippingByRoad');
+	define('_PLUGIN_SHOP_MI_SHIPPING_BYTRAIN', 'ShippingByTrain');
+	define('_PLUGIN_SHOP_MI_SHIPPING_BYPLANE', 'ShippingByPlane');
+	define('_PLUGIN_SHOP_MI_SHIPPING_EXPRESS', 'ShippingExpress');
+	define('_PLUGIN_SHOP_MI_SHIPPING_UNKNOWN', 'ShippingUnknown');
+	define('_PLUGIN_SHOP_MI_SHIPPING_MANUALPHONECALL', 'ShippingManualPhoneCall');
+	define('_PLUGIN_SHOP_MI_SHIPPING_EMAIL', 'ShippingEmail');
+	define('_PLUGIN_SHOP_MI_SHIPPING_APICALL', 'ShippingAPICall');
+	define('_PLUGIN_SHOP_MI_SHIPPING_FAX', 'ShippingFax');
+	define('_PLUGIN_SHOP_MI_SHIPPING_OTHER', 'ShippingOther');
+	
+	define('_PLUGIN_SHOP_MI_SHOPS_PHYSICALSTORE', 'ShopsPhysicalStore');
+	define('_PLUGIN_SHOP_MI_SHOPS_DELIVERYRUN', 'ShopsDelieveryRun');
+	define('_PLUGIN_SHOP_MI_SHOPS_SERVICECENTER', 'ShopsServiceCenter');
+	define('_PLUGIN_SHOP_MI_SHOPS_SERVICEWAREHOUSE', 'ShopsWarehouse');
+	define('_PLUGIN_SHOP_MI_SHOPS_SERVICE', 'ShopsService');
+	define('_PLUGIN_SHOP_MI_SHOPS_OTHER', 'ShopsOther');
+?>
---
+++ branches/mamba/xshop/plugin/orders.php
@@ -0,0 +1,292 @@
+<?php
+	// Orders Pre Insert of Orders Record plugin
+	function OrderPaidOrderOpenPreInsert($object) {
+		
+	}
+	function OrderUnpaidOrderOpenPreInsert($object) {
+		
+	}
+	function OrderCancelledOrderOpenPreInsert($object) {
+		
+	}
+	function OrderFraudPaidOrderOpenPreInsert($object) {
+		
+	}
+	function OrderFraudUnpaidOrderOpenPreInsert($object) {
+		
+	}
+	function OrderFraudCancelledOrderOpenPreInsert($object) {
+		
+	}
+	
+	function OrderPaidOrderClosedPreInsert($object) {
+		
+	}
+	function OrderUnpaidOrderClosedPreInsert($object) {
+		
+	}
+	function OrderCancelledOrderClosedPreInsert($object) {
+		
+	}
+	function OrderFraudPaidOrderClosedPreInsert($object) {
+		
+	}
+	function OrderFraudUnpaidOrderClosedPreInsert($object) {
+		
+	}
+	function OrderFraudCancelledOrderClosedPreInsert($object) {
+		
+	}
+	
+	function OrderPaidOrderCheckedOutPreInsert($object) {
+		
+	}
+	function OrderUnpaidOrderCheckedOutPreInsert($object) {
+		
+	}
+	function OrderCancelledOrderCheckedOutPreInsert($object) {
+		
+	}
+	function OrderFraudPaidOrderCheckedOutPreInsert($object) {
+		
+	}
+	function OrderFraudUnpaidOrderCheckedOutPreInsert($object) {
+		
+	}
+	function OrderFraudCancelledOrderCheckedOutPreInsert($object) {
+		
+	}
+	
+	function OrderPaidOrderGoneToInvoicePreInsert($object) {
+		
+	}
+	function OrderUnpaidOrderGoneToInvoicePreInsert($object) {
+		
+	}
+	function OrderCancelledOrderGoneToInvoicePreInsert($object) {
+		
+	}
+	function OrderFraudPaidOrderGoneToInvoicePreInsert($object) {
+		
+	}
+	function OrderFraudUnpaidOrderGoneToInvoicePreInsert($object) {
+		
+	}
+	function OrderFraudCancelledOrderGoneToInvoicePreInsert($object) {
+		
+	}
+	
+	function OrderPaidOrderOtherPreInsert($object) {
+		
+	}
+	function OrderUnpaidOrderOtherPreInsert($object) {
+		
+	}
+	function OrderCancelledOrderOtherPreInsert($object) {
+		
+	}
+	function OrderFraudPaidOrderOtherPreInsert($object) {
+		
+	}
+	function OrderFraudUnpaidOrderOtherPreInsert($object) {
+		
+	}
+	function OrderFraudCancelledOrderOtherPreInsert($object) {
+		
+	}
+	
+	
+	
+	// Order Records on Post Insert
+	function OrderPaidOrderOpenPostInsert($object) {
+		
+	}
+	function OrderUnpaidOrderOpenPostInsert($object) {
+		
+	}
+	function OrderCancelledOrderOpenPostInsert($object) {
+		
+	}
+	function OrderFraudPaidOrderOpenPostInsert($object) {
+		
+	}
+	function OrderFraudUnpaidOrderOpenPostInsert($object) {
+		
+	}
+	function OrderFraudCancelledOrderOpenPostInsert($object) {
+		
+	}
+	
+	function OrderPaidOrderClosedPostInsert($object) {
+		
+	}
+	function OrderUnpaidOrderClosedPostInsert($object) {
+		
+	}
+	function OrderCancelledOrderClosedPostInsert($object) {
+		
+	}
+	function OrderFraudPaidOrderClosedPostInsert($object) {
+		
+	}
+	function OrderFraudUnpaidOrderClosedPostInsert($object) {
+		
+	}
+	function OrderFraudCancelledOrderClosedPostInsert($object) {
+		
+	}
+	
+	function OrderPaidOrderCheckedOutPostInsert($object) {
+		
+	}
+	function OrderUnpaidOrderCheckedOutPostInsert($object) {
+		
+	}
+	function OrderCancelledOrderCheckedOutPostInsert($object) {
+		
+	}
+	function OrderFraudPaidOrderCheckedOutPostInsert($object) {
+		
+	}
+	function OrderFraudUnpaidOrderCheckedOutPostInsert($object) {
+		
+	}
+	function OrderFraudCancelledOrderCheckedOutPostInsert($object) {
+		
+	}
+	
+	function OrderPaidOrderGoneToInvoicePostInsert($object) {
+		
+	}
+	function OrderUnpaidOrderGoneToInvoicePostInsert($object) {
+		
+	}
+	function OrderCancelledOrderGoneToInvoicePostInsert($object) {
+		
+	}
+	function OrderFraudPaidOrderGoneToInvoicePostInsert($object) {
+		
+	}
+	function OrderFraudUnpaidOrderGoneToInvoicePostInsert($object) {
+		
+	}
+	function OrderFraudCancelledOrderGoneToInvoicePostInsert($object) {
+		
+	}
+	
+	function OrderPaidOrderOtherPostInsert($object) {
+		
+	}
+	function OrderUnpaidOrderOtherPostInsert($object) {
+		
+	}
+	function OrderCancelledOrderOtherPostInsert($object) {
+		
+	}
+	function OrderFraudPaidOrderOtherPostInsert($object) {
+		
+	}
+	function OrderFraudUnpaidOrderOtherPostInsert($object) {
+		
+	}
+	function OrderFraudCancelledOrderOtherPostInsert($object) {
+		
+	}
+	
+	
+	
+	// Order Records Get Plugins
+	function OrderPaidOrderOpenPostGet($object) {
+		
+	}
+	function OrderUnpaidOrderOpenPostGet($object) {
+		
+	}
+	function OrderCancelledOrderOpenPostGet($object) {
+		
+	}
+	function OrderFraudPaidOrderOpenPostGet($object) {
+		
+	}
+	function OrderFraudUnpaidOrderOpenPostGet($object) {
+		
+	}
+	function OrderFraudCancelledOrderOpenPostGet($object) {
+		
+	}
+	
+	function OrderPaidOrderClosedPostGet($object) {
+		
+	}
+	function OrderUnpaidOrderClosedPostGet($object) {
+		
+	}
+	function OrderCancelledOrderClosedPostGet($object) {
+		
+	}
+	function OrderFraudPaidOrderClosedPostGet($object) {
+		
+	}
+	function OrderFraudUnpaidOrderClosedPostGet($object) {
+		
+	}
+	function OrderFraudCancelledOrderClosedPostGet($object) {
+		
+	}
+	
+	function OrderPaidOrderCheckedOutPostGet($object) {
+		
+	}
+	function OrderUnpaidOrderCheckedOutPostGet($object) {
+		
+	}
+	function OrderCancelledOrderCheckedOutPostGet($object) {
+		
+	}
+	function OrderFraudPaidOrderCheckedOutPostGet($object) {
+		
+	}
+	function OrderFraudUnpaidOrderCheckedOutPostGet($object) {
+		
+	}
+	function OrderFraudCancelledOrderCheckedOutPostGet($object) {
+		
+	}
+	
+	function OrderPaidOrderGoneToInvoicePostGet($object) {
+		
+	}
+	function OrderUnpaidOrderGoneToInvoicePostGet($object) {
+		
+	}
+	function OrderCancelledOrderGoneToInvoicePostGet($object) {
+		
+	}
+	function OrderFraudPaidOrderGoneToInvoicePostGet($object) {
+		
+	}
+	function OrderFraudUnpaidOrderGoneToInvoicePostGet($object) {
+		
+	}
+	function OrderFraudCancelledOrderGoneToInvoicePostGet($object) {
+		
+	}
+	
+	function OrderPaidOrderOtherPostGet($object) {
+		
+	}
+	function OrderUnpaidOrderOtherPostGet($object) {
+		
+	}
+	function OrderCancelledOrderOtherPostGet($object) {
+		
+	}
+	function OrderFraudPaidOrderOtherPostGet($object) {
+		
+	}
+	function OrderFraudUnpaidOrderOtherPostGet($object) {
+		
+	}
+	function OrderFraudCancelledOrderOtherPostGet($object) {
+		
+	}
