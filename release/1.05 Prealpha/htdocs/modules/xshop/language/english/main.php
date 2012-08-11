62d9ff15894f268c2c8a9b0f5b2e3183702dc967
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- release/1.05 Prealpha/htdocs/modules/xshop/language/english/main.php
+++ release/1.05 Prealpha/htdocs/modules/xshop/language/english/main.php
@@ -1,38 +1,38 @@
 <?php
 
 	//Heading Defines
-	define('_SHOP_MN_CATEGORIES_H1', 'Categories');
-	define('_SHOP_MN_CATEGORIES_P', 'This is the current topical categories and subcategories.');
-	define('_SHOP_MN_SHOPS_H1', 'Shops');
-	define('_SHOP_MN_SHOPS_P', 'This is the current shops you can purchase from.');
-	define('_SHOP_MN_PRODUCTS_H1', 'Products');
-	define('_SHOP_MN_PRODUCTS_P', 'This is the current range of products.');
-	define('_SHOP_MN_MANUFACTURES_H1', 'Manufactures');
-	define('_SHOP_MN_MANUFACTURES_P', 'This is the current range of manufactures.');
+	define('_SHOP_MN_CATEGORIES_H1','Categories');
+	define('_SHOP_MN_CATEGORIES_P','This is the current topical categories and subcategories.');
+	define('_SHOP_MN_SHOPS_H1','Shops');
+	define('_SHOP_MN_SHOPS_P','This is the current shops you can purchase from.');
+	define('_SHOP_MN_PRODUCTS_H1','Products');
+	define('_SHOP_MN_PRODUCTS_P','This is the current range of products.');
+	define('_SHOP_MN_MANUFACTURES_H1','Manufactures');
+	define('_SHOP_MN_MANUFACTURES_P','This is the current range of manufactures.');
 	
 	
 	//Table Heading Defines
-	define('_SHOP_MN_SHOPS_LOGO', '');
-	define('_SHOP_MN_SHOPS_MENU_TITLE', 'Title');
-	define('_SHOP_MN_SHOPS_MENU_DESCRIPTION', 'Description');
-	define('_SHOP_MN_SHOPS_RANK', 'Rank');
-	define('_SHOP_MN_PRODUCTS_PICTURE', 'Image');
-	define('_SHOP_MN_PRODUCTS_CAT_NUMBER', 'Catalogue Number');
-	define('_SHOP_MN_PRODUCTS_STOCK', 'Stock');
-	define('_SHOP_MN_PRODUCTS_TYPE', 'Type');
-	define('_SHOP_MN_PRODUCTS_MENU_TITLE', 'Title');
-	define('_SHOP_MN_PRODUCTS_MENU_DESCRIPTION', 'Short Description');
-	define('_SHOP_MN_PRODUCTS_DISCOUNT', 'Discount');
-	define('_SHOP_MN_PRODUCTS_UNIT_PRICE', 'Unit Price');
-	define('_SHOP_MN_PRODUCTS_UNIT_WHOLESALE_PRICE', 'Wholesale Price');
-	define('_SHOP_MN_PRODUCTS_QUANITY_FOR_WHOLESALE', 'Quanity for Wholesale');
-	define('_SHOP_MN_PRODUCTS_CATEGORY', 'Category');
-	define('_SHOP_MN_PRODUCTS_MANUFACTURE', 'Manufacture');
-	define('_SHOP_MN_PRODUCTS_RANK', 'Rank');
-	define('_SHOP_MN_PRODUCTS_TAGS', 'Tags');
-	define('_SHOP_MN_PRODUCTS_PURCHASE', 'Purchase');
-	define('_SHOP_MN_MANUFACTURES_LOGO', '');
-	define('_SHOP_MN_MANUFACTURES_MENU_TITLE', 'Title');
-	define('_SHOP_MN_MANUFACTURES_MENU_DESCRIPTION', 'Description');
-	define('_SHOP_MN_MANUFACTURES_RANK', 'Rank');
+	define('_SHOP_MN_SHOPS_LOGO','');
+	define('_SHOP_MN_SHOPS_MENU_TITLE','Title');
+	define('_SHOP_MN_SHOPS_MENU_DESCRIPTION','Description');
+	define('_SHOP_MN_SHOPS_RANK','Rank');
+	define('_SHOP_MN_PRODUCTS_PICTURE','Image');
+	define('_SHOP_MN_PRODUCTS_CAT_NUMBER','Catalogue Number');
+	define('_SHOP_MN_PRODUCTS_STOCK','Stock');
+	define('_SHOP_MN_PRODUCTS_TYPE','Type');
+	define('_SHOP_MN_PRODUCTS_MENU_TITLE','Title');
+	define('_SHOP_MN_PRODUCTS_MENU_DESCRIPTION','Short Description');
+	define('_SHOP_MN_PRODUCTS_DISCOUNT','Discount');
+	define('_SHOP_MN_PRODUCTS_UNIT_PRICE','Unit Price');
+	define('_SHOP_MN_PRODUCTS_UNIT_WHOLESALE_PRICE','Wholesale Price');
+	define('_SHOP_MN_PRODUCTS_QUANITY_FOR_WHOLESALE','Quanity for Wholesale');
+	define('_SHOP_MN_PRODUCTS_CATEGORY','Category');
+	define('_SHOP_MN_PRODUCTS_MANUFACTURE','Manufacture');
+	define('_SHOP_MN_PRODUCTS_RANK','Rank');
+	define('_SHOP_MN_PRODUCTS_TAGS','Tags');
+	define('_SHOP_MN_PRODUCTS_PURCHASE','Purchase');
+	define('_SHOP_MN_MANUFACTURES_LOGO','');
+	define('_SHOP_MN_MANUFACTURES_MENU_TITLE','Title');
+	define('_SHOP_MN_MANUFACTURES_MENU_DESCRIPTION','Description');
+	define('_SHOP_MN_MANUFACTURES_RANK','Rank');
 	
